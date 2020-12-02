26f1ca4ac41eb478c3d366588d6c127862c64af3
Matthew Bamber <matthewbamber@icloud.com>
2020-12-02 09:05:54 +0000

    feat: Add solution for Day 2

---
+++ days/day_2/part1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"strconv"
+	"strings"
+)
+
+// Part1 solves the first part of the day's puzzle
+func Part1(ctx context.Context, input string) (interface{}, error) {
+	entries := strings.Split(strings.TrimSpace(input), "\n")
+
+	var valid int
+	for _, password := range entries {
+		ok, err := passwordIsValidP1(password)
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			valid++
+		}
+	}
+
+	return valid, nil
+}
+
+func passwordIsValidP1(p string) (bool, error) {
+	min, max, char, password, err := parsePassword(p)
+	if err != nil {
+		return false, err
+	}
+
+	numTimes := strings.Count(password, char)
+	return numTimes >= min && numTimes <= max, nil
+}
+
+func parsePassword(p string) (i, j int, char, password string, err error) {
+	parts := strings.Fields(p)
+	minmax := strings.Split(parts[0], "-")
+
+	i, err = strconv.Atoi(minmax[0])
+	if err != nil {
+		return 0, 0, "", "", err
+	}
+
+	j, err = strconv.Atoi(minmax[1])
+	if err != nil {
+		return 0, 0, "", "", err
+	}
+
+	char = strings.TrimSuffix(parts[1], ":")
+	password = parts[2]
+
+	return
+}
---
+++ days/day_2/part2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"strings"
+)
+
+// Part2 solves the second part of the day's puzzle
+func Part2(ctx context.Context, input string) (interface{}, error) {
+	entries := strings.Split(strings.TrimSpace(input), "\n")
+
+	var valid int
+	for _, password := range entries {
+		ok, err := passwordIsValidP2(password)
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			valid++
+		}
+	}
+
+	return valid, nil
+}
+
+func passwordIsValidP2(p string) (bool, error) {
+	i, j, char, password, err := parsePassword(p)
+	if err != nil {
+		return false, err
+	}
+
+	return (string(password[i-1]) == char) != (string(password[j-1]) == char), nil // A XOR B <-> A != B
+}
